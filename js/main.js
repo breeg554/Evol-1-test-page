5f5a18636a71be5eff0004dd351d348007be92c6
breeg554 <42748647+breeg554@users.noreply.github.com>
2019-09-12 14:02:41 +0200

    almost end mobile

--- js/main.js
+++ js/main.js
@@ -5,3 +5,41 @@ mobileBtn.addEventListener("click", () => {
   mobileBtn.classList.toggle("main-nav__mobile-btn--active");
   toggleMenu.classList.toggle("main-nav__container--show");
 });
+
+$(document).ready(function() {
+  $(".customers-slider__content__swiper").slick({
+    dots: false,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
