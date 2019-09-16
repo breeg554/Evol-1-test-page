2a493467bfb4db4d4aa6918cb55d450447ef2acb
breeg554 <42748647+breeg554@users.noreply.github.com>
2019-09-16 09:31:06 +0200

    smooth scroll

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
           slidesToShow: 3,
           slidesToScroll: 3,
           infinite: true,
-          dots: true
+          dots: false
         }
       },
       {
