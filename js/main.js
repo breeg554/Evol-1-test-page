2ae248aebd3e551aebb1b034395d85e9b1276a1b
breeg554 <42748647+breeg554@users.noreply.github.com>
2019-09-22 13:36:08 +0200

    work

--- js/main.js
+++ js/main.js
@@ -2,15 +2,36 @@ const mobileBtn = document.querySelector(".main-nav__mobile-btn");
 const toggleMenu = document.querySelector(".main-nav__container");
 
 mobileBtn.addEventListener("click", () => {
-  mobileBtn.classList.toggle("main-nav__mobile-btn--active");
+  mobileBtn.classList.toggle("main-nav__mobile-btn--active"); //mobile menu
   toggleMenu.classList.toggle("main-nav__container--show");
 });
 
 const mainNav = document.querySelector(".main-nav");
 window.addEventListener("scroll", () => {
   if (window.innerWidth >= 992 && window.scrollY <= 60) {
+    //change fixed menu
     mainNav.classList.add("main-nav--highest");
   } else mainNav.classList.remove("main-nav--highest");
+});
+
+const modalOpenBtn = document.querySelectorAll(".lightbox-btn");
+const backgroundModal = document.querySelector(".modal");
+const modalCloseBtn = document.querySelector(".modal__content__btn");
+const modalRowContent = document.querySelector(".modal-row");
+const modalContainerContent = document.querySelector(".modal-container");
+
+modalOpenBtn.forEach(btn =>
+  btn.addEventListener("click", () => {
+    modalRowContent.classList.add("modal-row--active");
+    modalContainerContent.classList.add("modal-container--active");
+    modalContainerContent.style.top = `${window.scrollY}px`;
+    backgroundModal.classList.add("modal--active");
+  })
+);
+modalCloseBtn.addEventListener("click", () => {
+  modalRowContent.classList.remove("modal-row--active");
+  backgroundModal.classList.remove("modal--active");
+  modalContainerContent.classList.remove("modal-container--active");
 });
 
 $(document).ready(function() {
